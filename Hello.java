b34379111f32d7c0646b8d52c298c6dbae4f4e1d
Shubham7 <shubhamchavan71195@gmail.com>
2025-11-29 09:50:18 +0530

    changes in dev

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello java");
         System.out.println("changes for dev");
+        System.out.println("changes for push");
     }
 }
